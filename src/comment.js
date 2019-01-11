45215d2c28d502680965c474110cd595250dd81e
sceptreCnsn <cansin.memis@clb.lcl>
2019-01-11 18:25:22 +0300

    Commit

--- src/comment.js
+++ src/comment.js
@@ -2,10 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const CommentSchema = new Schema({
-    content:String,
-    user:{type:Schema.Types.ObjectId,ref:'user'}
+  content: String,
+  user: { type: Schema.Types.ObjectId, ref: "user" }
 });
 
-const Comment = mongoose.model('comment',CommentSchema);
+const Comment = mongoose.model("comment", CommentSchema);
 
 module.exports = Comment;
